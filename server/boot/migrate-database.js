fd80d30a5f2bc6a2255fa2b0c015ecf325fe7153
ardev9905 <marioarce402@gmail.com>
2018-11-24 14:01:17 -0600

    nuevo parámetro en TypeUser

---
+++ server/boot/migrate-database.js
@@ -0,0 +1,95 @@
+module.exports = function(app) {
+    
+    /*app.dataSources.mysql.automigrate('AccessToken', function(err) {
+        if (err) throw err;
+        console.log('Models created: AccessToken');
+    });
+
+    app.dataSources.mysql.automigrate('ACL', function(err) {
+        if (err) throw err;
+        console.log('Models created: ACL');
+    });
+
+    app.dataSources.mysql.automigrate('RoleMapping', function(err) {
+        if (err) throw err;
+        console.log('Models created: RoleMapping');
+    });
+
+    app.dataSources.mysql.automigrate('Role', function(err) {
+        if (err) throw err;
+        console.log('Models created: Role');
+    });
+
+    app.dataSources.mysql.automigrate('Location', function(err) {
+        if (err) throw err;
+        console.log('Models created: Location');
+    });
+    
+    app.dataSources.mysql.automigrate('Cooperative', function(err) {
+        if (err) throw err;
+        console.log('Models created: Cooperative');
+    });*/
+    
+    //segundo bloque a migrar
+
+    /*app.dataSources.mysql.automigrate('TypeUser', function(err) {
+        if (err) throw err;
+        console.log('Models created: TypeUser');
+    });
+
+    app.dataSources.mysql.automigrate('User', function(err) {
+      if (err) throw err;
+      console.log('Models created: User');
+    });
+
+    app.dataSources.mysql.automigrate('TypeComment', function(err) {
+        if (err) throw err;
+        console.log('Models created: TypeComment');
+    });
+    
+    app.dataSources.mysql.automigrate('Comment', function(err) {
+        if (err) throw err;
+        console.log('Models created: Comment');
+    });
+    
+    app.dataSources.mysql.automigrate('TypeNews', function(err) {
+        if (err) throw err;
+        console.log('Models created: TypeNews');
+    });
+    
+    /*app.dataSources.mysql.automigrate('News', function(err) {
+        if (err) throw err;
+        console.log('Models created: News');
+    });
+    
+    app.dataSources.mysql.automigrate('Interlocal', function(err) {
+        if (err) throw err;
+        console.log('Models created: Interlocal');
+    });
+
+    /*app.dataSources.mysql.automigrate('PictureStop', function(err) {
+        if (err) throw err;
+        console.log('Models created: PictureStop');
+    });
+    
+    app.dataSources.mysql.automigrate('Route', function(err) {
+        if (err) throw err;
+        console.log('Models created: Route');
+    });
+    
+    app.dataSources.mysql.automigrate('Stop', function(err) {
+        if (err) throw err;
+        console.log('Models created: Stop');
+    });
+    
+    app.dataSources.mysql.automigrate('UserRoute', function(err) {
+        if (err) throw err;
+        console.log('Models created: UserRoute');
+    });
+    
+    app.dataSources.mysql.automigrate('UserCooperative', function(err) {
+        if (err) throw err;
+        console.log('Models created: UserCooperative');
+    });*/
+    
+};
