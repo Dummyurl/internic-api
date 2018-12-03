db269aa8326588e9169b6b65056f5139b51f9fe5
ardev9905 <marioarce402@gmail.com>
2018-12-03 11:27:13 -0600

    nuevo método remoto

--- common/models/type-news.js
+++ common/models/type-news.js
@@ -5,6 +5,9 @@ module.exports = function(Typenews) {
         if (this.createAt === undefined) {
           this.createAt = Date.now();
         }
+        if(this.id === 0 || this.id === null){
+          this.id = undefined;
+        }
         if (next) next();
       };
 };
