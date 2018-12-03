66cc3d028323f81c2ed8ac3aeb0c843f7274c7a6
ardev9905 <marioarce402@gmail.com>
2018-12-02 21:07:00 -0600

    nuevo método remoto

--- common/models/news.js
+++ common/models/news.js
@@ -4,6 +4,7 @@ module.exports = function(News) {
     News.afterInitialize = function(next) {
         if (this.createAt === undefined) {
           this.createAt = Date.now();
+          this.id = undefine;
         }
         if (next) next();
       };
