7ad155ca8afc790f0cc52e3768a832853043e06f
ardev9905 <marioarce402@gmail.com>
2018-12-03 12:31:11 -0600

    solución de errores

--- common/models/news.js
+++ common/models/news.js
@@ -4,7 +4,6 @@ module.exports = function(News) {
     News.afterInitialize = function(next) {
         if (this.createAt === undefined) {
           this.createAt = Date.now();
-          this.id = undefine;
         }
         if(this.id === 0 || this.id === null){
           this.id = undefined;
